ec7c5cdc1314f9273a00d903328f1bda8efe9311
tamthanh9701 <tamthanh9701@gmail.com>
2026-04-24 21:05:34 +0700

    Add align prop to DropdownMenuContent

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -51,12 +51,14 @@ DropdownMenuTrigger.displayName = "DropdownMenuTrigger";
 
 const DropdownMenuContent = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
+  React.HTMLAttributes<HTMLDivElement> & { align?: 'start' | 'center' | 'end' }
+>(({ className, align = 'end', ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
       "absolute right-0 z-50 mt-2 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      align === 'end' && "right-0",
+      align === 'start' && "left-0",
       className
     )}
     {...props}
